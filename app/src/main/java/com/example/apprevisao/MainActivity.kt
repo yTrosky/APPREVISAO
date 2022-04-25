16bdc136837d23ebe5ddebf2fe5238c4184f05c2
yTrosky <thiago.machado39@etec.sp.gov.br>
2022-04-25 12:08:31 -0300

    App revisão kotlin

---
+++ app/src/main/java/com/example/apprevisao/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.apprevisao
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{valMaiorMenor(it)}
+    }
+
+
+    private fun valMaiorMenor(view: View){
+
+        val editText = findViewById<EditText>(R.id.val_num)
+        val editText2 = findViewById<EditText>(R.id.val_numII)
+
+        if(Integer.parseInt(editText.text.toString()) == Integer.parseInt(editText2.text.toString()) {
+                Toast.makeText(this, "Os numeros são iguais", Toast.LENGTH_SHORT).show()
+            }else
+                if {Integer.parseInt(editText.text.toString()) > Integer.parseInt(editText2.text.toString()){
+            Toast.makeText(this, "O primeiro numero é maior", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Os numeros são iguais", Toast.LENGTH_SHORT).show()
+
+
+
+        }
+            }
+    }
+}
